f5c447e850b4497116e6a23e2d8f6e0803b7ae24
Snaacky <snaacky@gmail.com>
2018-05-24 10:48:17 -0400

    Revised codebase
    Pretty much same updates as all the other cheats but now we don't shoot teammates on this one.

--- config.py
+++
@@ -1,5 +0,0 @@
-# These values will change with every update.
-dwLocalPlayer = (0xAA8BBC)
-dwForceAttack = (0x2EC7E38)
-m_iCrosshairId = (0xB294)
-aim_key = "f"
--- sapphire.py
+++ sapphire.py
@@ -1,29 +1,37 @@
 import keyboard
 import pymem
 import pymem.process
-import time
-from config import *
+
+dwEntityList = (0x4A8473C)
+dwForceAttack = (0x2EC6AC8)
+dwLocalPlayer = (0xAA9AB4)
+m_fFlags = (0x100)
+m_iCrosshairId = (0xB2A4)
+m_iTeamNum = (0xF0)
 
 pm = pymem.Pymem("csgo.exe")
-
+client = pymem.process.module_from_name(pm.process_id, "client.dll").base_address
+trigger_key = "shift"
 
 def main():
-    print("Sapphire has launched. Your trigger key is: {}.".format(aim_key))
-
-    client = pymem.process.module_from_name(pm.process_id, "client.dll")
-    player = client.base_address + dwLocalPlayer
-    in_crosshair = pm.read_int(player) + m_iCrosshairId
-    force_attack = client.base_address + dwForceAttack
-
+    print("Sapphire has launched.")
+    shooting = False
     while True:
-        result = pm.read_int(in_crosshair)
-
-        if keyboard.is_pressed(aim_key):
-            if result > 0 and result <= 64:
-                pm.write_int(force_attack, 5)
-                time.sleep(0.01)  # Revolver won't fire without delay.
-                pm.write_int(force_attack, 4)
-
+        player = pm.read_int(client + dwLocalPlayer)
+        if keyboard.is_pressed(trigger_key):
+            entity = pm.read_int(player + m_iCrosshairId)
+            if entity > 0 and entity <= 64:
+                entity = pm.read_int(client + dwEntityList + (entity -1) * 0x10)
+                entity_team = pm.read_int(entity + m_iTeamNum)
+                player_team = pm.read_int(player + m_iTeamNum)
+                    
+                if player_team != entity_team:
+                    shooting = True
+                    pm.write_int(client + dwForceAttack, 5)
+            
+        if not keyboard.is_pressed(trigger_key) and shooting == True:
+            pm.write_int(client + dwForceAttack, 4)
+            shooting = False
 
 if __name__ == '__main__':
     main()
