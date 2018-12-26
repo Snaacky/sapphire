e9a8e00e6092376eddd0ed7e1f3d81dbed52213f
Snaacky <snaacky@gmail.com>
2018-12-26 15:44:29 -0500

    Fixed issue that broke code, updated offsets, added little attempt at future-proofing
    Pymem updated and I didn't think to leave a version in requirements.txt.

--- sapphire.py
+++ sapphire.py
@@ -2,15 +2,15 @@ import keyboard
 import pymem
 import pymem.process
 
-dwEntityList = (0x4A8473C)
-dwForceAttack = (0x2EC6AC8)
-dwLocalPlayer = (0xAA9AB4)
-m_fFlags = (0x100)
-m_iCrosshairId = (0xB2A4)
-m_iTeamNum = (0xF0)
+dwEntityList = (0x4CCDBFC)
+dwForceAttack = (0x30FF2A0)
+dwLocalPlayer = (0xCBD6B4)
+m_fFlags = (0x104)
+m_iCrosshairId = (0xB394)
+m_iTeamNum = (0xF4)
 
 pm = pymem.Pymem("csgo.exe")
-client = pymem.process.module_from_name(pm.process_id, "client.dll").base_address
+client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 trigger_key = "shift"
 
 def main():
@@ -18,8 +18,10 @@ def main():
     shooting = False
     while True:
         player = pm.read_int(client + dwLocalPlayer)
+
         if keyboard.is_pressed(trigger_key):
             entity = pm.read_int(player + m_iCrosshairId)
+
             if entity > 0 and entity <= 64:
                 entity = pm.read_int(client + dwEntityList + (entity -1) * 0x10)
                 entity_team = pm.read_int(entity + m_iTeamNum)
