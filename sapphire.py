5148557b19fc20eb8b0118634186f514f2d4eae6
Snaacky <Snaacky@pm.me>
2020-04-20 11:59:41 -0400

    refactor: 💡 Adjusted ALT-TAB check

--- sapphire.py
+++ sapphire.py
@@ -20,23 +20,24 @@ def main():
     client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 
     while True:
-        if GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
-            player = pm.read_int(client + dwLocalPlayer)
-
-            if keyboard.is_pressed(trigger_key):
-                entity_id = pm.read_int(player + m_iCrosshairId)
-                entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 0x10)
-
-                entity_team = pm.read_int(entity + m_iTeamNum)
-                player_team = pm.read_int(player + m_iTeamNum)
-
-                if entity_id > 0 and entity_id <= 64 and player_team != entity_team:
-                    pm.write_int(client + dwForceAttack, 6)
-
-                time.sleep(0.006)
-
         if not keyboard.is_pressed(trigger_key):
             time.sleep(0.1)
+
+        if not GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
+            continue
+
+        if keyboard.is_pressed(trigger_key):
+            player = pm.read_int(client + dwLocalPlayer)
+            entity_id = pm.read_int(player + m_iCrosshairId)
+            entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 0x10)
+
+            entity_team = pm.read_int(entity + m_iTeamNum)
+            player_team = pm.read_int(player + m_iTeamNum)
+
+            if entity_id > 0 and entity_id <= 64 and player_team != entity_team:
+                pm.write_int(client + dwForceAttack, 6)
+
+            time.sleep(0.006)
 
 
 if __name__ == '__main__':
