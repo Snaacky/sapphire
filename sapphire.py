5e999eb838096b1dd8b903ee319b5d4973d603e2
Snaacky <Snaacky@pm.me>
2020-04-20 11:09:48 -0400

    fix: 🐛 Reduced CPU usage, no longer fires while ALT-TAB'd, etc

--- sapphire.py
+++ sapphire.py
@@ -1,39 +1,43 @@
 import keyboard
 import pymem
 import pymem.process
+import time
+from win32gui import GetWindowText, GetForegroundWindow
 
-dwEntityList = (0x4CCDBFC)
-dwForceAttack = (0x30FF2A0)
-dwLocalPlayer = (0xCBD6B4)
+dwEntityList = (0x4D43AB4)
+dwForceAttack = (0x3175088)
+dwLocalPlayer = (0xD2FB84)
 m_fFlags = (0x104)
-m_iCrosshairId = (0xB394)
+m_iCrosshairId = (0xB3D4)
 m_iTeamNum = (0xF4)
 
-pm = pymem.Pymem("csgo.exe")
-client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 trigger_key = "shift"
+
 
 def main():
     print("Sapphire has launched.")
-    shooting = False
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
+
     while True:
-        player = pm.read_int(client + dwLocalPlayer)
+        if GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
+            player = pm.read_int(client + dwLocalPlayer)
 
-        if keyboard.is_pressed(trigger_key):
-            entity = pm.read_int(player + m_iCrosshairId)
+            if not keyboard.is_pressed(trigger_key):
+                time.sleep(0.1)
 
-            if entity > 0 and entity <= 64:
-                entity = pm.read_int(client + dwEntityList + (entity -1) * 0x10)
+            if keyboard.is_pressed(trigger_key):
+                entity_id = pm.read_int(player + m_iCrosshairId)
+                entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 0x10)
+
                 entity_team = pm.read_int(entity + m_iTeamNum)
                 player_team = pm.read_int(player + m_iTeamNum)
-                    
-                if player_team != entity_team:
-                    shooting = True
-                    pm.write_int(client + dwForceAttack, 5)
-            
-        if not keyboard.is_pressed(trigger_key) and shooting == True:
-            pm.write_int(client + dwForceAttack, 4)
-            shooting = False
+
+                if entity_id > 0 and entity_id <= 64 and player_team != entity_team:
+                    pm.write_int(client + dwForceAttack, 6)
+
+                time.sleep(0.006)
+
 
 if __name__ == '__main__':
     main()
