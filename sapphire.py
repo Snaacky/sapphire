efa64f3c6ad0fbe2116c9f6b61dab705d4628c86
Tri <67373913+TriDiscord@users.noreply.github.com>
2021-04-20 22:27:41 +0300

    Update sapphire.py

--- sapphire.py
+++ sapphire.py
@@ -1,15 +1,14 @@
 import keyboard
 import pymem
-import pymem.process
 import time
 from win32gui import GetWindowText, GetForegroundWindow
 
-dwEntityList = (0x4D4B104)
-dwForceAttack = (0x317C6EC)
-dwLocalPlayer = (0xD36B94)
-m_fFlags = (0x104)
-m_iCrosshairId = (0xB3D4)
-m_iTeamNum = (0xF4)
+dwEntityList = 81411996
+dwForceAttack = 52253916
+dwLocalPlayer = 14205628
+m_iCrossHairID = 46052
+m_iTeamNum = 244
+m_fFlags = 260
 
 trigger_key = "shift"
 
@@ -29,12 +28,12 @@ def main():
         if keyboard.is_pressed(trigger_key):
             player = pm.read_int(client + dwLocalPlayer)
             entity_id = pm.read_int(player + m_iCrosshairId)
-            entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 0x10)
+            entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 16)
 
             entity_team = pm.read_int(entity + m_iTeamNum)
             player_team = pm.read_int(player + m_iTeamNum)
 
-            if entity_id > 0 and entity_id <= 64 and player_team != entity_team:
+            if 64 >= entity_id > 0 and player_team != entity_team:
                 pm.write_int(client + dwForceAttack, 6)
 
             time.sleep(0.006)
