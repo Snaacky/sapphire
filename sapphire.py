743b976cd6fff660e401ecb854130c3fffc6aa29
Tim Eifler <tim.eif@protonmail.com>
2023-01-16 11:04:28 +0100

    Update sapphire.py
    Changed variable names to clearer names

--- sapphire.py
+++ sapphire.py
@@ -4,14 +4,14 @@ import pymem.process
 import time
 from win32gui import GetWindowText, GetForegroundWindow
 
-dwEntityList = (0x4D4B104)
-dwForceAttack = (0x317C6EC)
-dwLocalPlayer = (0xD36B94)
-m_fFlags = (0x104)
-m_iCrosshairId = (0xB3D4)
-m_iTeamNum = (0xF4)
+ENTITY_LIST_ADDRESS = (0x4D4B104)
+FORCE_ATTACK_ADDRESS = (0x317C6EC)
+LOCAL_PLAYER_ADDRESS = (0xD36B94)
+PLAYER_FLAGS_OFFSET = (0x104)
+CROSSHAIR_ID_OFFSET = (0xB3D4)
+PLAYER_TEAM_OFFSET = (0xF4)
 
-trigger_key = "shift"
+TRIGGER_KEY = "shift"
 
 
 def main():
@@ -20,25 +20,11 @@ def main():
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
     while True:
-        if not keyboard.is_pressed(trigger_key):
+        if not keyboard.is_pressed(TRIGGER_KEY):
             time.sleep(0.1)
 
         if not GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
             continue
 
-        if keyboard.is_pressed(trigger_key):
-            player = pm.read_int(client + dwLocalPlayer)
-            entity_id = pm.read_int(player + m_iCrosshairId)
-            entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 0x10)
-
-            entity_team = pm.read_int(entity + m_iTeamNum)
-            player_team = pm.read_int(player + m_iTeamNum)
-
-            if entity_id > 0 and entity_id <= 64 and player_team != entity_team:
-                pm.write_int(client + dwForceAttack, 6)
-
-            time.sleep(0.006)
-
-
-if __name__ == '__main__':
-    main()
+        if keyboard.is_pressed(TRIGGER_KEY):
+            local_player = pm.read_int(client + LOCAL_PLAYER_ADDRESS)
